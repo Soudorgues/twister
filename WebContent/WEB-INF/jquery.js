be0bc65f0f465d6db083152eb714722a8d30a659
3521139 <3521139@ppti-14-308-15.ufr-info-p6.jussieu.fr>
2018-03-27 15:03:40 +0200

    javascript + jquery

---
+++ WebContent/WEB-INF/jquery.js
@@ -0,0 +1,134 @@
+function connexion(form) {
+    var login = form.login.value;
+    var password = form.password.value;
+    var ok = verif_formulaire_connexion(login, password);
+    if (ok) {
+        connect(login, password);
+    }
+}
+
+function verif_formulaire_connexion(login, password) {
+    if (login.length == 0 || password.length == 0) {
+        error_login("Pb connexion");
+        return false;
+    }
+    if (login.length > 20) {
+        error_login("login > 20");
+        return false;
+    }
+    return true;
+}
+
+function error_login(msg) {
+    var msg_box = "<div id=\"msg_error_connexion\">" + msg + "</div>";
+    var old_msg = $("#msg_error_connexion");
+    if (old_msg.length == 0) {
+        $("#form").prepend(msg_box);
+    }
+    else {
+        old_msg.replaceWith(msg_box);
+    }
+    $("#msg_error_connexion").css({"color": "red"});
+}
+/*
+    makeConnexionPanel()
+    $("body").html(s)    remplace
+    $("body").append(s)  ajoute à la fin
+    $("body").prepend(s) ajoute au début
+*/
+function makeConnexionPanel() {
+    var s = "<div id=\"connexion_main\"> ... <div id=\"link_enregistrement\" \
+        onClick=\"javascript:makeEnregistrementPanel()\">...</div>";
+    $("body").html(s);
+}
+
+function makeEnregistrementPanel() {
+    var s = "<div></div>";
+    $("body").html(s);
+}
+
+function connect(login, password) {
+    console.log("connexion" + login + " " + password);
+    var idUser = 78;
+    var key = "ABCDEFGH";
+    if (!noConnexion) {
+        return false;
+    }
+    else {
+        reponseConnexion({"key": key, "id": idUser, "login": login,
+            "follow": [2]});
+    }
+}
+
+function responseConnexion(rep) {
+    if (rep.erreur = undefined) {
+        env.key = rep.key;
+        env.id = rep.id;
+        env.login = rep.login;
+        env.follows = new Set();
+    }
+    for (var i = 0; i < rep.follows.length; i++) {
+        env.follows.add(rep.follows[i]);
+    }
+    if (noConnexion) {
+        follows[rep.id] = new Set();
+        for (var i = 0; i < rep.follows.length; i++) {
+            follows[rep.id].add(rep.follows[i]);
+        }
+    }
+    makeMainPanel(); // td 07
+    error_login(rep.erreur);
+}
+
+/*
+    enregistrement
+    responseConnexion (makeMainPanel)
+*/
+
+function developpeMessage(id) {
+    var m = env.msg[id];
+    var elmt = $("#message_"+id+".comments");
+    for (var i = 0; i < m.length; i++) {
+        var c = m.comments[i];
+        elmt.append(c.getHTML());
+    }
+    elmt = $("#message_"+id+".new_comments");
+    elmt.append("<form name=\"new_comment_form\" id=\"new_comment_form\" \
+        action=\"javascript:func_new_comment("+id+")\">");
+    $("#message_"+id+"img").replaceWith("<img src=\"...\" onClick=\" \
+        javascript:plieMessage("+id+")\"/>");
+}
+
+function plieMessage(id) {
+    var m = env.msg[id];
+    var elmt = $("#message_"+id+".comments");
+    elmt.html(" ");
+    $("#message_"+id+".img").replaceWith("<img src=\"...\" onClick=\" \
+        javascript:developpeMessage("+id+")\"/>");
+}
+
+function newComment(id) {
+    var text = $("#new_"+id).val();
+    if (!noConnexion) {
+        return;
+    }
+    else {
+        newComment_reponse(id, JSON.Stringify(
+            new Commentaire(env.msg[id].comments.length+1,{"id":env.idlogin...})))
+    }
+}
+
+function newComment_response(id, rep) {
+    var com = JSON.parse(rep, revival);
+    if (com != undefined && com.erreur == undefined) {
+        var elmt = $("#message_"+id+".comments");
+        elmt.append(com.getHTML());
+        env.msg[i].comments.push(com);
+        if (noConnexion) {
+            localdb[id] = env.msg[id];
+        }
+    }
+    else {
+        alert(comm.error);
+    }
+}
---
+++ WebContent/WEB-INF/main.js
@@ -0,0 +1,144 @@
+function Message(id, login, text, date, comments) {
+    this.id = id;
+    this.auteur = login;
+    this.text = text;
+    this.date = date;
+    if (comments == undefined) {
+        comments = [];
+    }
+    this.comments = comments;
+}
+
+Message.prototype.getHTML = function() {
+    return "<div id=\"message_" + this.id + "\" \
+    class=\"message\"> \
+    <div class=\"text_message\"> \
+    " + this.text + "<div class=\"infos_message\"> \
+    <span> Post de " + this.auteur + "\
+    </span></div></div></div>";
+}
+
+function Commentaire(id, auteur, text, date) {
+    this.id = id;
+    this.auteur = login;
+    this.text = text;
+    this.date = date;
+}
+
+Commentaire.prototype.getHTML = function() {
+    return "<div id=\"message_" + this.id + "\" \
+    class=\"message\"> \
+    <div class=\"text_message\"> \
+    " + this.text + "<div class=\"infos_message\"> \
+    <span> Post de " + this.auteur + "\
+    </span></div></div></div>";
+}
+
+
+function revival1(key, value) {
+    if (value.comment != undefined) {
+        var c = new Message(value.id, value.auteur, value.text, value.date, value.comments);
+        return c;
+    }
+    else if (value.text != undefined) {
+        var c = new Commentaire(value.id, value.auteur, value.text, value.date);
+        return c;
+    }
+    else if (key == date) {
+        var d = new Date(value);
+        return d;
+    }
+    return value;
+}
+
+function SetVirtualDB() {
+    localdb = [];
+    follows = [];
+    var u1 = {"id": 1, "login": "sly"};
+    var u2 = {"id": 2, "login": "joe"};
+    var u3 = {"id": 3, "login": "luc"};
+}
+
+function init() {
+    noConnection = true;
+    env = new Object();
+    SetVirtualDB();
+    var m1 = new Message(12, "vinc", "Bonjour", new Date(), undefined);
+    var m2 = new Message(13,"doums","neymar",new Date(),undefined);
+    var tab = [m1, m2];
+
+    for (var i = 0; i < tab.length; i++) {
+        //s = s + tab[i].getHTML();
+        $("#main").append(tab[i]);
+    }
+    //document.getElementById("main").innerHTML = s;
+}
+
+function makeMainPanel(fromId, fromLogin, query) {
+    env.messages = [];
+    if (fromID == undefined) {
+        fromId = 1;
+    }
+    env.fromID = fromID;
+    env.fromLogin = fromLogin;
+    console.log(env.fromLogin);
+    env.query = query;
+    var s = "<header id=\"top\">";
+    if (env.fromID < 0) {
+        s += "<div id=\"title\"> Actualités</div>";
+    }
+    else {
+        if (!env.follows.has(env.fromID)) {
+            s += "<div id=\"title\"> Page de " + fromLogin + "<div \
+            class=\"add\"><img src=\"Images/add.png\" title=\"suivre\" \
+            onclick= javascript:follow()\"></div></div>";
+        }
+        else {
+            s += "<div id=\"title\"> Page de " + fromLogin + "<div \
+            class=\"add\"><img src=\"Images/remove.png\" \
+            onclick= javascript:stopfollow()\"></div></div>";
+        }
+    }
+    s += "</div><div id=\"connect\"><span id=\"log\" \
+    onclick=\"javascript.pageUser(" + env.id + "," + env.login + ")\"> \
+    <img src=\"Images/logout.png\" \
+            onclick= javascript:logout()\"></div></div>";
+    return s;
+}
+
+function completeMessages() {
+    if (!noConnection) {
+
+    } else {
+        var tab = getFromLocalDB(env.fromId, -1, env.minId, 1);
+        completeMessagesResponse(JSON.stringify(tab));
+    }
+}
+
+function completeMessagesResponse(rep) {
+    var tab = JSON.parse(rep, revival1);
+    for (var i = 0; i < tab.length; i++) {
+        var m = tab[i];
+        alert(m.getHTML());
+        env.messages[m.id] = m;
+        if (m.id > env.maxId) {
+            env.maxId = m.id;
+        }
+        if (env.minId < 0 || m.id < env.minId) {
+            env.minId = m.id;
+        }
+    }
+}
+
+function getFromLocalDB(from, minId, maxId, nbMax) {
+    var tab = [];
+    var nb = 0;
+    var f = new Set();
+    if (from > 0) {
+        f = follows[from];
+    }
+    for (var i = localdb.length - 1; i >= 0; i--) {
+        messages += i;
+    }
+    return tab
+}
