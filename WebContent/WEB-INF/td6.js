98ca78616fdfe2b3b2fabe329912c5df56871df1
3300613 <amadou.bah1@etu.upmc.fr>
2018-04-03 15:38:52 +0200

    tme9

---
+++ WebContent/WEB-INF/td6.js
@@ -0,0 +1,79 @@
+function Message(id, login, text, date, comments) {
+    this.id = id;
+    this.auteur = login;
+    this.text = text;
+    this.date = date;
+    if (comments == undefined) {
+        comments = [];
+    }
+    this.comments = comments;
+}
+
+Message.prototype.getHTML = function() {
+    return "<div id=\"message_" + this.id + "\" \
+    class=\"message\"> \
+    <div class=\"text_message\"> \
+    " + this.text + "<div class=\"infos_message\"> \
+    <span> Post de " + this.auteur + "\
+    </span></div></div></div>";
+}
+
+function Commentaire(id, auteur, text, date) {
+    this.id = id;
+    this.auteur = login;
+    this.text = text;
+    this.date = date;
+}
+
+Commentaire.prototype.getHTML = function() {
+    return "<div id=\"commentaire_" + this.id + "\" \
+    class=\"commentaire\"> \
+    <div class=\"text_commentaire\"> \
+    " + this.text + "<div class=\"infos_message\"> \
+    <span> Post de " + this.auteur + "\
+    </span></div></div></div>";
+}
+
+
+function revival1(key, value) {
+    if (value.comment != undefined) {
+        var c = new Message(value.id, value.auteur, value.text, value.date, value.comments);
+        return c;
+    }
+    else if (value.text != undefined) {
+        var c = new Commentaire(value.id, value.auteur, value.text, value.date);
+        return c;
+    }
+    else if (key == date) {
+        var d = new Date(value);
+        return d;
+    }
+    return value;
+}
+
+function SetVirtualDB() {
+    localdb = [];
+    follows = [];
+    var u1 = {"id": 1, "login": "sly"};
+    var u2 = {"id": 2, "login": "joe"};
+    var u3 = {"id": 3, "login": "luc"};
+}
+
+function init() {
+    noConnection = true;
+    env = new Object();
+    SetVirtualDB();
+    var m1 = new Message(12, "vinc", "Bonjour", new Date(), undefined);
+    var m2 = new Message(12, "Amadou", "hello", new Date(), undefined);    
+    var m3 = new Message(12, "prof",  "recu",   new Date(), undefined);
+    var tab = [m1, m2, m3];
+    var s=" ";
+    for (var i = 0; i < tab.length; i++) {
+        s = s + tab[i].getHTML();
+        //$("#main").append(tab[i]);
+    }
+    document.getElementById("listMessage").innerHTML = s;
+}
+
+
+init();
